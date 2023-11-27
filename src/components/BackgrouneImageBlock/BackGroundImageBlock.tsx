0f915d4534149fe528379685d1dd488d876d14bc
Баталов Михаил <batalov94@gmail.com>
2023-11-27 17:00:41 +0600

    Added sidebar

---
+++ src/components/BackgrouneImageBlock/BackGroundImageBlock.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './BackgroundImageBlock.css'; 
+
+const BackgroundImageBlock: React.FC = () => {
+  return (
+    <div className="background-image-block">
+    </div>
+  );
+};
+
+export default BackgroundImageBlock;
