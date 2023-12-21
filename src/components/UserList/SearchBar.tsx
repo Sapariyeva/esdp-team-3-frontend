1d54338e2a7f2ff3b47ea42e5303c14aca742b1c
batalovmv <batalov94@gmail.com>
2023-12-21 16:00:27 +0600

    добавлена пагинация

--- src/components/UserList/SearchBar.tsx
+++ src/components/UserList/SearchBar.tsx
@@ -1,36 +1,82 @@
-import React, { useState } from 'react';
-import { Input, Select } from 'antd';
+import React, { useContext, useState } from 'react';
+import { ConfigProvider, Input, Select } from 'antd';
+import { ERole } from '@/enum/role.enum';
+import { EUserStatus } from '@/enum/user.enum';
+
 const { Search } = Input;
 const { Option } = Select;
 
-interface SearchBarProps {
-    onSearch: (field: string, value: string) => void;
-    setSearchField: React.Dispatch<React.SetStateAction<string>>;
+interface ISearchBarProps {
+    onSearch: (searchField?: string, searchTerm?: string, status?: EUserStatus | undefined, role?: ERole | undefined) => void;
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
-    const [selectedField, setSelectedField] = useState('displayName'); 
+const SearchBar: React.FC<ISearchBarProps> = ({ onSearch }) => {
+    const [searchField, setSearchField] = useState<string | undefined>();
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [status, setStatus] = useState<EUserStatus | undefined>();
+    const [role, setRole] = useState<ERole | undefined>();
+    
+    const handleSearchFieldChange = (value: string) => {
+        setSearchField(value);
+    };
+
+    const handleStatusChange = (value: EUserStatus) => {
+        setStatus(value);
+    };
+
+    const handleRoleChange = (value: ERole) => {
+        setRole(value);
+    };
+
     const handleSearch = (value: string) => {
-        onSearch(selectedField, value);
+      
+        setSearchTerm(value);
+        onSearch(searchField, value, status, role);
     };
 
     return (
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', gap: '10px' }}>
             <Select
-                defaultValue="displayName"
-                style={{ width: 200, marginRight: 8 }}
-                onChange={setSelectedField} 
-                size="large"    
+                placeholder="Поиск"
+                style={{ width: 200 }}
+                onChange={handleSearchFieldChange}
+                value={searchField}
             >
                 <Option value="displayName">Display Name</Option>
+                <Option value="email">Email</Option>
                 <Option value="phone">Phone</Option>
-                <Option value="identifyingNumber">identifying Number</Option>
+                <Option value="identifyingNumber">Identifying Number</Option>
             </Select>
+
+            <Select
+                placeholder="Select Status"
+                style={{ width: 200 }}
+                onChange={handleStatusChange}
+                value={status}
+            >
+                {/* Плейсхолдер сейчас не нужен как Option, так как он уже установлен в самом Select */}
+                {Object.values(EUserStatus).map((statusValue) => (
+                    <Option key={statusValue} value={statusValue}>{statusValue}</Option>
+                ))}
+            </Select>
+
+            <Select
+                placeholder="Select Role"
+                style={{ width: 200 }}
+                onChange={handleRoleChange}
+                value={role}
+            >
+                {/* То же самое применяется и здесь */}
+                {Object.values(ERole).map((roleValue) => (
+                    <Option key={roleValue} value={roleValue}>{roleValue}</Option>
+                ))}
+            </Select>
+
+            {/* Поисковая строка */}
             <Search
                 placeholder="Enter search term..."
-                onSearch={handleSearch} 
-                enterButton
-                size="large"
+                onSearch={handleSearch}
+                style={{ flexGrow: 1 }}
             />
         </div>
     );
--- src/containers/Users/UsersPage.tsx
+++ src/containers/Users/UsersPage.tsx
@@ -3,41 +3,108 @@ import { fetchUsers } from '@/app/userList.slice';
 import SearchBar from '@/components/UserList/SearchBar';
 import { useAppDispatch, useAppSelector } from '@/app/store';
 import UserTable from '@/components/UserList/UserTable';
-import { IUser } from '@/interfaces/user.interface';
+import { EUserStatus } from '@/enum/user.enum';
+import { ERole } from '@/enum/role.enum';
+interface Filters {
+    offset: number;
+    limit: number;
+    phone?: string;
+    email?: string;
+    role?: string;
+    status?: string;
+    [key: string]: any;
+}
+
 
 const UsersPageContainer = () => {
     const dispatch = useAppDispatch();
+    const { userList, totalItems, totalPages, paginationLinks: links } = useAppSelector((state) => state.users);
+    const [currentPage, setCurrentPage] = useState(1);
+    const pagLinks = useAppSelector((state) => state.users.paginationLinks);
     const users = useAppSelector((state) => state.users.userList);
-    const [searchField, setSearchField] = useState('displayName');
-    const [searchTerm, setSearchTerm] = useState('');
-
+    const [filters, setFilters] = useState<Filters>({
+        offset: 0,
+        limit: 10,
+        role: undefined,
+        status: undefined, 
+    });
+   
+    
+  
+    const fetchUsersWithUrl = (url: any) => {
+        dispatch(fetchUsers(url));
+    };
     useEffect(() => {
-        dispatch(fetchUsers());
-    }, [dispatch]);
-    const handleSearch = (field: string, term: string) => {
-        setSearchField(field);
-        setSearchTerm(term);
-    };
+        const requestFilters = Object.entries(filters).reduce((acc, [key, value]) => {
+            if (value !== undefined) acc[key] = value;
+            return acc;
+        }, {} as Filters);
+       
+        dispatch(fetchUsers(requestFilters));
+      
+    }, [dispatch, filters, currentPage]);
+    const handlePageChange = (newPage: number) => {
+        setCurrentPage(newPage); // Обновляем номер текущей страницы
 
-    const getFilteredUsers = (): IUser[] => {
-        if (!searchTerm) return users;
-        return users.filter((user) => {
-            const key = searchField as keyof IUser; 
-            const fieldValue = user[key]; 
-            return fieldValue !== null && typeof fieldValue === 'string' && fieldValue.toLowerCase().includes(searchTerm.toLowerCase());
-        });
     };
-    const filteredUsers = getFilteredUsers();
 
     
-   
+
+    const handleSearch = (searchField?: string, searchTerm?: string, status?: EUserStatus, role?: ERole) => {
+        const newFilters: Filters = {
+            ...filters,
+            offset: 0, // Сброс пагинации на первую страницу при новом поиске
+        };
+
+        // Удаляем предыдущие критерии поиска 
+        delete newFilters.displayName;
+        delete newFilters.email;
+        delete newFilters.phone;
+        delete newFilters.identifyingNumber;
+
+        if (searchTerm && searchField) {
+            // Применяем поисковый термин к выбранному полю
+            newFilters[searchField] = searchTerm;
+        }
+
+        // Обновляем role и status только если они выбраны
+        if (role) {
+            newFilters.role = role;
+        } else {
+            delete newFilters.role;
+        }
+
+        if (status) {
+            newFilters.status = status;
+        } else {
+            delete newFilters.status;
+        }
+
+        console.log(``, newFilters);
+        setFilters(newFilters);
+    };
+
 
     return (
         <div>
-            <SearchBar onSearch={handleSearch} setSearchField={setSearchField} />
-            <UserTable users={filteredUsers} />
+            <SearchBar onSearch={handleSearch} />
+            <UserTable
+                users={userList}
+                totalItems={totalItems}
+                totalPages={totalPages}
+                currentPage={currentPage}
+                links={{
+                    next: links.next,
+                    prev: links.prev,
+                    first: links.first,
+                    last: links.last,
+                }}
+                pageSize={filters.limit}
+                fetchUsers={fetchUsersWithUrl}
+            />
         </div>
     );
+
 };
 
 export default UsersPageContainer;
